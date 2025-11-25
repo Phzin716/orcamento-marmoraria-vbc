0672c387bd6764b7978ac9c3eef474eeba1510ca
Phzin716 <165541471+Phzin716@users.noreply.github.com>
2025-11-24 22:57:39 -0300

    Create App.jsx

---
+++ public/src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 App React rodando com Vite!</h1>
+      <p>Se você está vendo isso, tudo funcionou!</p>
+    </div>
+  )
+}
+
+export default App
